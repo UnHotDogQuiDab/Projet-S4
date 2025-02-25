815cb5934bc9311e712de2e8b1d0a7e5ed349264
Finaritra Raharinesy <finaritra.raharinesy@epita.fr>
2025-02-25 12:07:27 +0100

    try gtk4 pour voir si c'est mieux

---
+++ projet-s4/src/interface/interface.rs
@@ -0,0 +1,97 @@
+use gtk::prelude::*;
+use gtk::{
+    Application, ApplicationWindow, Button, FileChooserDialog, FileFilter,
+};
+use std::cell::RefCell;
+
+fn build_interface(app: &Application) 
+{
+    let window = ApplicationWindow::builder()
+        .application(app)
+        .title("Audio Compressor/Decompressor")
+        .default_width(400)
+        .default_height(200)
+        .build();
+
+    let vbox = gtk::Box::new(gtk::Orientation::Vertical, 5);
+    let btn_compress = Button::with_label("Compress audio file...");
+    let btn_decompress = Button::with_label("Decompress audio file...");
+
+    let selected_file = RefCell::new(None);
+
+    let open_file_dialog = |window: &ApplicationWindow, file_type: &str| 
+	{
+        let dialog = FileChooserDialog::new(
+            Some("Select File"),
+            Some(window),
+            gtk::FileChooserAction::Open,
+        );
+
+        //filters
+        let filter = FileFilter::new();
+        if file_type == "audio" 
+		{
+            filter.add_mime_type("audio/*");
+            filter.set_name(Some("Audio Files"));
+        } 
+		else if file_type == "compressed" 
+		{
+            filter.add_pattern("*.zip");
+            filter.add_pattern("*.rar");
+            filter.add_pattern("*.tar.gz");
+            filter.set_name(Some("Compressed Files"));
+        }
+        dialog.add_filter(filter);
+
+        dialog.add_buttons(&[
+            ("Open", gtk::ResponseType::Accept),
+            ("Cancel", gtk::ResponseType::Cancel),
+        ]);
+
+        let result = if dialog.run() == gtk::ResponseType::Accept 
+		{
+            dialog.file().and_then(|f| f.path())
+        } 
+		else 
+		{
+            None
+        };
+        dialog.close();
+        result
+    };
+
+    //open file to compress
+    btn_compress.connect_clicked(move |_| 
+	{
+        if let Some(path) = open_file_dialog(&window, "audio") 
+		{
+            *selected_file.borrow_mut() =
+                Some(path.to_string_lossy().into_owned());
+            println!("Compressing file: {} ...", path.display());
+        } 
+		else 
+		{
+            println!("No file selected.");
+        }
+    });
+
+    //open file to decompress
+    btn_decompress.connect_clicked(move |_| 
+	{
+        if let Some(path) = open_file_dialog(&window, "compressed") 
+		{
+            *selected_file.borrow_mut() =
+                Some(path.to_string_lossy().into_owned());
+            println!("Decompressing file: {} ...", path.display());
+        } 
+		else 
+		{
+            println!("No file selected.");
+        }
+    });
+
+    vbox.append(&btn_compress);
+    vbox.append(&btn_decompress);
+    window.set_child(Some(&vbox));
+    window.show();
+}
