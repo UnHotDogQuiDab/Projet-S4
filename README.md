e2af2d0ee1c0790d5df7147a22ca0fbcec25434d
Finaritra Raharinesy <finaritra.raharinesy@epita.fr>
2025-02-25 19:06:50 +0100

    c'est bon c'est relié

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ Projet S4
 
 Y'a un dossier par sous-projet, mais changez si vous avez besoin
 Si vous avez des fichiers sur lesquels vous testez votre code, go tous les mettre au même endroit (dans le dossier à ce nom)
+
+Run le main sur les pc d'epita : 
+->	RUSTUP_TOOLCHAIN=stable cargo run --release
--- projet-s4/src/main.rs
+++ projet-s4/src/main.rs
@@ -3,10 +3,6 @@ use gtk::Application;
 use gtk::prelude::ApplicationExt;
 use gtk::prelude::ApplicationExtManual;
 use projet_s4::interface::interface_gtk::build_interface;
-use crate::algo::compression::compression;
-use crate::algo::decompression::decompression;
-
-use std::env;
 
 fn main() 
 {
@@ -16,21 +12,4 @@ fn main()
 
     app.connect_activate(|app| build_interface(app));
     app.run();
-	
-    let args: Vec<String> = env::args().collect();
-    
-    if args.len() < 3 
-    {
-        println!("Needs more args");
-        return;
-    }
-
-    let input_file = &args[1];
-    let compressed_file = &args[2];
-    let output_file = &args[3];
-
-    compression(input_file, compressed_file);
-    println!("\n\n--------------------OBALIIIIIIIIE!!!!!!! (ça a compressé)--------------------\n\n");
-    decompression(compressed_file, output_file);
-    println!("\n\n--------------------TOGEPIIIIIIII!!!!!!! (ça a décompressé)--------------------\n\n");
 }
